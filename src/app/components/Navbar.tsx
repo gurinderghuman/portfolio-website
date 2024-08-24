96e5d1f7309102307bef1a3e901f0d1e859650d4
gurinderghuman <gurinder@ghumanportfolio.com>
2024-08-24 08:18:38 +0200

    chore: Update Navbar background color to green

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4">
+    <nav className="bg-green-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-xl font-bold">My Portfolio</Link>
         <ul className="flex space-x-4">
