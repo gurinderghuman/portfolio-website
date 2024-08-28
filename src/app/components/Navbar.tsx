5f4e92f3e4fe917396c573e6d6a741fe52b18a51
gurinderghuman <gurinder@ghumanportfolio.com>
2024-08-28 12:30:07 +0200

    update Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 text-white p-4 mt-8">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-xl font-bold">My Portfolio</Link>
+        <Link href="/" className="text-xl font-bold">Gurinder Ghuman</Link>
         <ul className="flex space-x-4">
           <li><Link href="/">Home</Link></li>
           <li><Link href="/about">About</Link></li>
