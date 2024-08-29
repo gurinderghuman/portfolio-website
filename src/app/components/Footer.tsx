db3f2115238e6fdd6fa4d4d7179c34f1c28d1144
gurinderghuman <gurinder@ghumanportfolio.com>
2024-08-29 08:05:55 +0200

    update footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 mt-8">
+    <footer className="bg-blue-800 text-white p-4 mt-8">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Gurinder Ghuman My Portfolio. All rights reserved.</p>
       </div>
     </footer>
   );
